16ea7366138c933397473096ef0560aa1e81263f
TAABADM1 <dmitry.baryshnikov@swisscom.com>
2025-12-30 13:22:38 +0100

    libs.versions.toml and build.gradle.kts: updated libs and sdk versions numbers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.expensetracker"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.expensetracker"
         minSdk = 34
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
